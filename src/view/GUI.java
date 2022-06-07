b7e59c4f6f97171de240ebb25c4e710f83e75b65
robinjoo1015 <robinjoo1015@naver.com>
2022-06-07 20:50:52 +0900

    Change gui

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -10,9 +10,16 @@ public class GUI extends JFrame {
     ArrayList<Player> playerList;
     public GUI(GameMap gameMap, ArrayList<Player> playerList) {
         setTitle("Bridge Game GUI");
-        setSize(500, 500);
+        setSize(1000, 1000);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
         setVisible(true);
+
+
+
         this.playerList = playerList;
     }
+
+
 }
